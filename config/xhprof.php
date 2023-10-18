67af03529a03012b7d59689ef68749d3825d36d4
Jamie Schouten <j4mie@hey.com>
2023-10-18 23:49:05 +0200

    Add sensible default config values, update readme usage with sail. (#7)

--- config/xhprof.php
+++ config/xhprof.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'enabled' => (bool) env('XHPROF_ENABLED'),
-    'endpoint' => (string) env('PROFILER_ENDPOINT'),
+    'enabled' => (bool) env('XHPROF_ENABLED', false),
+    'endpoint' => (string) env('PROFILER_ENDPOINT', 'http://127.0.0.1:8000/api/profiler/store'),
 ];
