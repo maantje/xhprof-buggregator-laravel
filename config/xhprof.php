b1f44d9f081c3deef48f2b81d54aec58aaddda08
Artur Melikbekian <Artur.Melikbekian@artwin.pro>
2024-06-27 15:56:49 +0300

    Add option to disable middleware. When we try to profile some part of code.

--- config/xhprof.php
+++ config/xhprof.php
@@ -2,5 +2,6 @@
 
 return [
     'enabled' => (bool) env('XHPROF_ENABLED', false),
+    'on_startup' => (bool) env('XHPROF_ON_STARTUP', true),
     'endpoint' => (string) env('PROFILER_ENDPOINT', 'http://127.0.0.1:8000/api/profiler/store'),
 ];
